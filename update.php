d95ebf61ee768dae80d3c079584f386946a42e1f
deepak vishwakarma <deepakvishwakarma9870@gmail.com>
2022-11-03 00:26:44 +0530

    modified:   update.php

--- update.php
+++ update.php
@@ -1,12 +1,18 @@
 <?php
-
-$m->MongoClient();
-$db = $m->selectDB('TYITDB1');
-$collection = $db->selectCollection('TYITCOL');
-// update data
-$collection->update(
-    array("name" => "Deepak"),
-    array('$set' => array( "age" => 35))
-);
+$m -> new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$db = $m -> selectDB('TYITDB1');
+$collection = $db -> selectCollection('TYITCOL');
+$collection -> update(array("name" => "Deepak"), array('$set' => array("name" => "Deepak", "age" => 23, "RollNo" => 229749, "Project" => "Guess what?")));
 echo "Data updated successfully";
 ?>
+
+
+// $m->MongoClient();
+// $db = $m->selectDB('TYITDB1');
+// $collection = $db->selectCollection('TYITCOL');
+// // update data
+// $collection->update(
+//     array("name" => "Deepak"),
+//     array('$set' => array( "age" => 35))
+// );
+// echo "Data updated successfully";
